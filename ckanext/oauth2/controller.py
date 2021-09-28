f4f47cab8a95f2478a8a7c35d7753d04cc1a3eed
Mark Gee <jw.ziggee@gmail.com>
2021-09-28 19:28:44 +0800

    switch to log warn

--- ckanext/oauth2/controller.py
+++ ckanext/oauth2/controller.py
@@ -41,8 +41,7 @@ class OAuth2Controller(base.BaseController):
         self.oauth2helper = oauth2.OAuth2Helper()
 
     def login(self):
-        print("logging in")
-        log.debug('login')
+        log.warn('login')
 
         # Log in attemps are fired when the user is not logged in and they click
         # on the log in button
