9214d173d147f6098baefcc5b753c617eb662002
Mark Gee <jw.ziggee@gmail.com>
2021-09-28 22:07:27 +0800

    move get previous page

--- ckanext/oauth2/blueprints.py
+++ ckanext/oauth2/blueprints.py
@@ -1,12 +1,31 @@
 from flask import Blueprint
 import ckanext.oauth2.oauth2 as oauth2
 import ckanext.oauth2.constants as constants
-from ckanext.oauth2.plugin import _get_previous_page
 
 
 oauth2_blueprint = Blueprint('oauth2', __name__)
 
 oauth2_helper = oauth2.OAuth2Helper()
+
+def _get_previous_page(default_page):
+    if 'came_from' not in toolkit.request.params:
+        came_from_url = toolkit.request.headers.get('Referer', default_page)
+    else:
+        came_from_url = toolkit.request.params.get('came_from', default_page)
+
+    came_from_url_parsed = urlparse(came_from_url)
+
+    # Avoid redirecting users to external hosts
+    if came_from_url_parsed.netloc != '' and came_from_url_parsed.netloc != toolkit.request.host:
+        came_from_url = default_page
+
+    # When a user is being logged and REFERER == HOME or LOGOUT_PAGE
+    # he/she must be redirected to the dashboard
+    pages = ['/', '/user/logged_out_redirect']
+    if came_from_url_parsed.path in pages:
+        came_from_url = default_page
+
+    return came_from_url
 
 def login():
     log.debug('login')
--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -65,27 +65,6 @@ def request_reset(context, data_dict):
     return _no_permissions(context, msg)
 
 
-def _get_previous_page(default_page):
-    if 'came_from' not in toolkit.request.params:
-        came_from_url = toolkit.request.headers.get('Referer', default_page)
-    else:
-        came_from_url = toolkit.request.params.get('came_from', default_page)
-
-    came_from_url_parsed = urlparse(came_from_url)
-
-    # Avoid redirecting users to external hosts
-    if came_from_url_parsed.netloc != '' and came_from_url_parsed.netloc != toolkit.request.host:
-        came_from_url = default_page
-
-    # When a user is being logged and REFERER == HOME or LOGOUT_PAGE
-    # he/she must be redirected to the dashboard
-    pages = ['/', '/user/logged_out_redirect']
-    if came_from_url_parsed.path in pages:
-        came_from_url = default_page
-
-    return came_from_url
-
-
 class OAuth2Plugin(plugins.SingletonPlugin):
 
     plugins.implements(plugins.IAuthenticator, inherit=True)
